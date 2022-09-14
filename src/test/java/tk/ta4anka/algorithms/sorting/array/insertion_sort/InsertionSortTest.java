a45b3013ce1e6c060cda93730adaafe02c3d8217
ta4anka <vladymirsheyko@gmail.com>
2022-09-14 03:41:55 +0200

    added test for insertion sort

---
+++ src/test/java/tk/ta4anka/algorithms/sorting/array/insertion_sort/InsertionSortTest.java
@@ -0,0 +1,19 @@
+package tk.ta4anka.algorithms.sorting.array.insertion_sort;
+
+import org.junit.jupiter.api.Test;
+import tk.ta4anka.algorithms.sorting.array.shell_sort.ShellSort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertionSortTest {
+
+    @Test
+    void sort() {
+        int[] input = {20, 35, -15, 7, 55, 1, -22};
+        int[] sortedArray = InsertionSort.sort(input);
+
+        int[] expected = {-22, -15, 1, 7, 20, 35, 55};
+
+        assertArrayEquals(expected, sortedArray);
+    }
+}
